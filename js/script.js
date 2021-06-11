3d4b32a2d5ff18d7101001f05da30f27d3a3703a
Aaron Scofield <tuj83098@temple.edu>
2021-06-11 10:55:12 -0400

    added interaction pane, additional reset button

--- js/script.js
+++ js/script.js
@@ -3,21 +3,33 @@ var parameters = {
     data: [{
         fn: 'sin(x)',
         color: 'blue'
-    }
-        ],
+    }],
     grid: true,
     yAxis: {domain: [-1, 1]},
     xAxis: {domain: [0, 2*Math.PI]}
 };
 
-function plot(){
+function reset(){
+    parameters.data[0].fn = "sin(x)";
+    parameters.xAxis.domain = [0, 2*Math.PI];
+    parameters.yAxis.domain = [-1, 1];
+    parameters.data[0].color = "blue";
+
     functionPlot(parameters);
 }
 
-function function1(){
-    document.getElementById("heading1").innerHTML = "Or is it??";
-}
+function plot(){
+    var xMin = document.querySelector("#xMin").value;
+    var xMax = document.querySelector("#xMax").value;
+    var yMin = document.querySelector("#yMin").value;
+    var yMax = document.querySelector("#yMax").value;
+    var color = document.querySelector("#color").value;
+    var f = document.querySelector("#function").value;
 
-function myFunction(){
-    pagetitle.style.color = "blueviolet";
+    parameters.data[0].fn = f;
+    parameters.xAxis.domain = [xMin, xMax];
+    parameters.yAxis.domain = [yMin, yMax];
+    parameters.data[0].color = color;
+
+    functionPlot(parameters);
 }
