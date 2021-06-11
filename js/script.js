c3da65794f991f40df7c7cd96e8f3a516e68d4c7
Aaron Scofield <tuj83098@temple.edu>
2021-06-11 10:12:54 -0400

    added graphing implementation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-function function1() {
+var parameters = {
+    target: "#myFunction",
+    data: [{
+        fn: 'sin(x)',
+        color: 'blue'
+    }
+        ],
+    grid: true,
+    yAxis: {domain: [-1, 1]},
+    xAxis: {domain: [0, 2*Math.PI]}
+};
+
+function plot(){
+    functionPlot(parameters);
+}
+
+function function1(){
     document.getElementById("heading1").innerHTML = "Or is it??";
 }
 
-function myFunction() {
+function myFunction(){
     pagetitle.style.color = "blueviolet";
 }
